271bca5c41fe0b22c8ca2873abbb02d0b1e1712a
Laksh <raftaarmalhotra@gmail.com>
2026-01-16 23:29:21 +0530

    starting main log code with OOPs

---
+++ countwordfreq.py
@@ -0,0 +1,19 @@
+def count(dict):
+    count = 0
+    for key in dict:
+        count += 1
+    return count
+
+
+dict = {"brand": "Ford", "model": "Mustang","year": 1964}
+print(count(dict))
+
+
+
+thisdict = {
+  "brand": "Ford",
+  "model": "Mustang",
+  "year": 1964,
+  "year": 2020
+}
+print(thisdict)
---
+++ mainlogparseroop.py
@@ -0,0 +1,46 @@
+import re
+
+class LogEntry:
+    def __init__(self, date: str, time: str, level: str, message: str):
+        self.date = date
+        self.time = time
+        self.level = level
+        self.message = message
+
+    def __str__(self):
+        return f"[{self.date} {self.time} {self.level}: {self.message}]"
+
+class InvalidLogFormat(Expection):
+    pass
+    
+class LogParser:
+    log_patter = re.compile(
+        r"(?P<date>\d{4}-\d{2}-\d{2}) "
+        r"(?P<time>\d{2}:\d{2}:\d{2}) "
+        r"(?P<level>[A-Z]+) "
+        r"(?P<message>.*)"
+    )
+
+    def parse(self, line: str) -> LogEntry:
+
+
+def main():
+    reader = FileReader("logs.txt")
+    parser = LogParser()
+
+    entries[]
+
+    for line in reader.read_lines():
+        try:
+            entry = parser.parse(line)
+            entries.append(entry)
+        except InvalidLogFormat as e:
+            print("skipping bad line: ", e)
+
+    print("Parsed entries:")
+    for e in entries:
+        print(e)
+
+
+if __name__ == "__main__":
+    main()
